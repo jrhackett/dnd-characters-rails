34c35c8ff7f8a31161c100ed715ea7a70db12cc6
Jacob Hackett <jacobrhackett@gmail.com>
2017-03-06 10:14:58 -0500

    fixing index view for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
-  resources :users, :only => [:show]
+  resources :users, :only => [:index, :show]
 
   devise_scope :user do
     get '/login', to: 'devise/sessions#new'
